d36a165628032d1be4f40ff9ebb975bd018bc181
DimensionalScoop <m@pernklau.eu>
2025-02-18 22:09:37 +0100

    add basic support for horizontal walls (but the code is ugly)

--- horizontal_walls.py
+++ horizontal_walls.py
@@ -23,6 +23,10 @@ def fill_center(transforms, x, y, h):
     return hull()(*_get_posts(transforms, x, y, h))
 
 
+def fill_edge(transforms, x, y, h, dir="x"):
+    return hull()(*_get_edge_posts(transforms, x, y, h, dir))
+
+
 def _get_posts(transforms, x, y, h):
     assert len(transforms) == 4
     post = cylinder(h, r=eps)
@@ -32,23 +36,52 @@ def _get_posts(transforms, x, y, h):
         yield trf(at_edge)
 
 
-transforms = [
-    lambda o: o.rotateX(20).rotateY(5).color("Pink"),
-    lambda o: o.rotateX(25).rotateY(5).forward(x + 2).up(8).color("Green"),
-    lambda o: o.rotateX(40).right(x + 12).forward(x + 2).up(27),
-    lambda o: o.rotateX(30).right(x + 7).up(10).color("Blue"),
-]
+def _get_edge_posts(transforms, x, y, h, dir="x"):
+    assert len(transforms) == 2
+    post = cylinder(h, r=eps)
+
+    match dir:
+        case "y":
+            for dx, trf in zip((-1, 1), transforms[::-1]):
+                for dy in (-1, 1):
+                    at_edge = post.forward(dx * x / 2).right(dy * y / 2).down(z / 2)
+                    yield trf(at_edge)
+        case "x":
+            for dy, trf in zip((-1, 1), transforms[::-1]):
+                for dx in (-1, 1):
+                    at_edge = post.forward(dx * x / 2).right(dy * y / 2).down(z / 2)
+                    yield trf(at_edge)
+        case _:
+            raise NotImplementedError()
+
+
+demo_transforms = np.array(
+    [
+        lambda o: o.rotateX(20).rotateY(5).color("Pink"),
+        lambda o: o.rotateX(25).rotateY(5).forward(x + 2).up(8).color("Green"),
+        lambda o: o.rotateX(40).right(x + 12).forward(x + 2).up(27),
+        lambda o: o.rotateX(30).right(x + 7).up(10).color("Blue"),
+    ]
+)
 
 
 @render
 def dev():
-    return fill_center(transforms, x, x, z)
+    return union()(
+        *[
+            fill_center(demo_transforms, x, x, z),
+            fill_edge(demo_transforms[[0, 1]], x, x, z, "y"),
+            fill_edge(demo_transforms[[1, 2]], x, x, z, "x"),
+            fill_edge(demo_transforms[[2, 3]][::-1], x, x, z, "y"),
+            fill_edge(demo_transforms[[0, 3]], x, x, z, "x"),
+        ]
+    )
 
 
 @render
 def example():
     shape = cube(x, x, z, center=True)
-    switches = [tr(shape) for tr in transforms]
+    switches = [tr(shape) for tr in demo_transforms]
     return np.sum(switches).background()
 
 
