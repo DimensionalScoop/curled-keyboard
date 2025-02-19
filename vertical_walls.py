a54409bc19ec4bdf03f5ca46180dbdddd9a847c5
DimensionalScoop <m@pernklau.eu>
2025-02-19 09:12:41 +0100

    clean up vertical wall code, increase convexity

--- vertical_walls.py
+++ vertical_walls.py
@@ -1,15 +1,7 @@
 from solid2 import *  # noqa: F403
-# from solid2.extensions.bosl2 import *  # noqa: F403
+from solid2.extensions.bosl2 import bounding_box, xcyl
 
-# import solid2.extensions.bosl2 as bosl  # noqa: F403
-import solid2 as sp
-from solid2.extensions.bosl2 import TOP, bounding_box, minkowski_difference, xcyl
-
-from mommy import render, render_all
-from scad_helpers import attachable_cube
-
-import numpy as np
-from numpy import array as ar
+from mommy import render_all
 
 
 eps = 0.01
@@ -23,30 +15,18 @@ def make_wall(
     fill_blocker=None,
 ):
     walls = simple_walls_cutoff(roof, base_offset, thickness).color("Gray")
-    gapfill = gapfill(roof, base_offset, thickness, fill_blocker).color("Lime")
-    return walls + gapfill
-
-
-def _vertical_walls(roof, base_offset, thickness=2):
-    raise NotImplementedError()
-    base = _create_base(roof).down(base_offset)
-
-    bb = bounding_box(excess=thickness)(roof, base)
-
-    wallmaker = cylinder(eps, d=thickness, _fn=6)
-    wallmaker = sphere(thickness)
-    closed_shell = minkowski()(roof, wallmaker) - roof
-    base_cutout = _create_base(roof, inf).down(base_offset) & bb
-
-    return closed_shell - base_cutout
+    fill = gapfill(roof, base_offset, thickness, fill_blocker).color("Lime")
+    return walls + fill
 
 
 def simple_walls(roof, base_offset, thickness=2):
     """Create vertical walls around the `roof`.
     Walls go up to the maximum height of the roof."""
     base = roof.projection()
-    solid_walls = base.offset(thickness).linear_extrude(inf).down(base_offset)
-    cutout = base.linear_extrude(inf).down(base_offset)
+    solid_walls = (
+        base.offset(thickness).linear_extrude(inf, convexity=10).down(base_offset)
+    )
+    cutout = base.linear_extrude(inf, convexity=10).down(base_offset)
     thin_walls = solid_walls - cutout
     bb = bounding_box(excess=thickness)(roof + base).down(thickness)
 
@@ -71,7 +51,7 @@ def gapfill(
 
     walls = simple_walls_cutoff(roof, base_offset, thickness)
     wall_base = walls.projection()
-    wall_base_3d = wall_base.linear_extrude(eps)
+    wall_base_3d = wall_base.linear_extrude(eps, convexity=10)
 
     # covers everything, even stuff we don't want to fill
     glob = hull()(wall_base_3d, roof)
@@ -79,53 +59,42 @@ def gapfill(
     glob = minkowski()(glob, cylinder(eps, eps, 0))
 
     # don't add material outside of the walls
-    glob &= (roof + walls).projection().linear_extrude(inf).down(eps)
+    glob &= (roof + walls).projection().linear_extrude(inf, convexity=5).down(eps)
 
     if fill_blocker is not None:
         # anywhere where walls might exist, gapfill is allowed to be
-        glob -= fill_blocker - wall_base.offset(eps).linear_extrude(inf)
+        glob -= fill_blocker - wall_base.offset(eps).linear_extrude(inf, convexity=5)
     else:
-        glob -= roof.projection().offset(delta=eps).linear_extrude(inf).down(eps)
+        glob -= (
+            roof.projection()
+            .offset(delta=eps)
+            .linear_extrude(inf, convexity=5)
+            .down(eps)
+        )
 
     return glob
-
-
-def _make_printable():
-    raise NotImplementedError()
-    fill_without_walls = glob  # - wall_base.linear_extrude(inf).down(-eps)
-    bottom_surface = fill_without_walls.down(eps).projection(cut=True)
-
-    # make a stair case
-    supports = union()()
-    for z in np.arange(0, 5):
-        step = bottom_surface.offset(delta=-z).linear_extrude(z).down(z)
-        supports += step
-
-    # h = 4
-    # printable_round = cylinder(h, 0, 3)
-    # supports = (
-    #     minkowski_difference()(bottom_surface, printable_round)
-    #     # .linear_extrude(eps)
-    #     # .up(eps / 2)
-    # ).down(h)
 
 
 def _create_base(roof, h=eps):
     return roof.projection().linear_extrude(h)
 
 
-@render
 def example():
     roof = cube([17, 17, 8.5]).rotateX(30)
     roof -= xcyl(17, 8).up(10)
     cleanup = cube([17, 17, inf]).down(inf / 2).rotateX(30)
     # roof += cylinder(h=5, r=9).forward(15)
 
-    walls = simple_walls_cutoff(roof, 5).color("Gray")
-    fill = gapfill(roof, 5, 2, cleanup).color("Lime")
+    # walls = simple_walls_cutoff(roof, 5).color("Gray")
+    # fill = gapfill(roof, 5, 2, cleanup).color("Lime")
 
-    return fill + roof + walls
+    base_height = 5
+    wall_thickness = 2
+    walls = make_wall(roof, base_height, wall_thickness, cleanup)
+
+    return roof + walls
 
 
 if __name__ == "__main__":
-    render_all()
+    ex = example()
+    render_all(ex)
