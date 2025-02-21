19603bb281e9408a88aae04122d371fc50235ee4
DimensionalScoop <m@pernklau.eu>
2025-02-21 14:06:46 +0100

    start adding supports to the inside of the socket

--- assembly.py
+++ assembly.py
@@ -19,6 +19,7 @@ import numpy as np
 from numpy import array as ar
 
 import horizontal_walls
+import chain_wall
 
 
 eps = 0.01
@@ -51,12 +52,54 @@ for x in range(grid.shape[0]):
         grid[x, y] = _transform
 
 
+socket_dummy = cube(x_width, x_width, z_height, center=True)
+socket = import_stl("./switch-mount.stl").down(z_height / 2)
+
+
+def remove_above(body, tool):
+    """remove all mass of the body that is in or above the tool"""
+    # XXX: only works for convex shapes
+    return body - hull()(tool, tool.up(inf))
+
+
+def create_key_support(place):
+    to_bottom = lambda o: o.down(z_height / 2)
+    column = place(to_bottom(cylinder(z_height, r=2)))
+
+    # head = place(sphere(0.5)).color("Green")
+    head = socket.up(z_height / 2).down(eps).projection(cut=True)
+    head = head.offset(delta=-0.5)
+    head = to_bottom(head.linear_extrude(eps))
+    head = place(head)
+
+    head = hull()(head, column)
+
+    plate = place(to_bottom(cylinder(eps, r=2)))
+    stem = plate.projection().linear_extrude(inf)
+    stem = stem.down(inf / 2)
+    stem = remove_above(stem, place(socket_dummy))
+
+    return column + stem + head
+
+
 @render
+def example_support():
+    sample_transform = grid[3, 3]
+
+    socket = horizontal_walls.make_switches(np.array([sample_transform]), False)
+
+    support = create_key_support(sample_transform)
+
+    return socket.debug() + support.color("DarkBlue")
+
+
 def example():
-    switches = horizontal_walls.make_switches(grid)
+    switches = horizontal_walls.make_switches(grid, False)
     fill = horizontal_walls.fill_between_switches(grid)
 
-    return switches + fill
+    wall = chain_wall.create_switch_wall(grid, 1)
+
+    return wall + switches + fill
 
 
 if __name__ == "__main__":
